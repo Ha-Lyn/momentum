a11e73563a4c40dd0164104e9520b019304ffadd
João Lucas Rodrigues <54967700+joaosp7@users.noreply.github.com>
2026-09-05 06:53:58 -0300

    cleaning

--- app/src/preload.ts
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onRecordingState: (callback: (state: { active: boolean }) => void) => {
-    ipcRenderer.on('recording-state', (_event, value) => callback(value));
-  }
-});
